e9d1fe5293aa5a99809c45124938361215460165
prokash <pprokash102@gmail.com>
2022-06-17 01:31:09 +0600

    service section update

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Services = () => {
   return (
-    <div className="bg-black">
+    <div className="bg-black text-white">
       <div className="flex ">
         <div className="bg-green-500 w-40 md:w-60 h-24 flex justify-center items-center">
           <h2 className="font-semibold text-xl text-white uppercase">
@@ -10,6 +10,90 @@ const Services = () => {
           </h2>
         </div>
         <div className="w-0 h-0 border-b-green-500 border-b-[96px] border-r-transparent border-r-[96px] border-l-transparent border-l-[96px] rotate-180 -ml-24 -z-5"></div>
+      </div>
+      <div className="flex flex-col md:flex-row md:gap-8 gap-3 justify-center items-center md:py-20 py-5 w-[90%] mx-auto lg:w-[1080px]">
+        <div className="p-5 ">
+          <svg className="-z-50" height="150" width="200">
+            <text
+              fill="none"
+              stroke="#fff"
+              fontSize="250"
+              fontWeight="bold"
+              x="100"
+              y="200"
+              textAnchor="middle"
+            >
+              1
+            </text>
+          </svg>
+          <div className="bg-black  z-20">
+            <h2 className="text-5xl font-bold my-3">Preciese</h2>
+            <p>
+              Curabitur cursus mattis ligula a maximus pellentesque in purus
+              malesuada pharetra eros.
+            </p>
+            <div className="relative">
+              <button className="border-2 hover:border-green-500 hover:text-green-500 group rounded-full h-10 duration-500 w-32 hover:w-40 my-7">
+                More
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="p-5 ">
+          <svg className="-z-50" height="150" width="200">
+            <text
+              fill="none"
+              stroke="#fff"
+              fontSize="250"
+              fontWeight="bold"
+              x="100"
+              y="200"
+              textAnchor="middle"
+            >
+              2
+            </text>
+          </svg>
+          <div className="bg-black  z-20">
+            <h2 className="text-5xl font-bold my-3">Support</h2>
+            <p>
+              Curabitur cursus mattis ligula a maximus pellentesque in purus
+              malesuada pharetra eros.
+            </p>
+            <div className="relative">
+              <button className="border-2 hover:border-green-500 hover:text-green-500 group rounded-full h-10 duration-500 w-32 hover:w-40 my-7">
+                More
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-5 ">
+          <svg className="-z-50" height="150" width="200">
+            <text
+              fill="none"
+              stroke="#fff"
+              fontSize="250"
+              fontWeight="bold"
+              x="100"
+              y="200"
+              textAnchor="middle"
+            >
+              3
+            </text>
+          </svg>
+          <div className="bg-black  z-20">
+            <h2 className="text-5xl font-bold my-3">Responsive</h2>
+            <p>
+              Curabitur cursus mattis ligula a maximus pellentesque in purus
+              malesuada pharetra eros.
+            </p>
+            <div className="relative">
+              <button className="border-2 hover:border-green-500 hover:text-green-500 group rounded-full h-10 duration-500 w-32 hover:w-40 my-7">
+                More
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
